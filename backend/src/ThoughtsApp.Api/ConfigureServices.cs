345938b7795c90e6c807bee51d59cc1ed963557b
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-10-14 11:30:55 +0200

    ➕ add: packages, Serilog, development process documentation

---
+++ backend/src/ThoughtsApp.Api/ConfigureServices.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+namespace ThoughtsApp.Api;
+
+internal static class ConfigureServices
+{
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.AddSerilog();
+    }
+    
+    /// <summary>
+    /// Configures Serilog then adds it to the DI container
+    /// </summary>
+    private static void AddSerilog(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSerilog((services, loggerConfiguration) =>
+        {
+            // load configuration from appsettings.json
+            loggerConfiguration.ReadFrom.Configuration(builder.Configuration)
+                // integrates Serilog with DI
+                // this is required for custom sinks or enrichers that
+                //  depennd on application services
+                .ReadFrom.Services(services);
+        });
+    }
+}
--- backend/src/ThoughtsApp.Api/Program.cs
+++ backend/src/ThoughtsApp.Api/Program.cs
@@ -1,44 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Events;
+using ThoughtsApp.Api;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.AddServices();
+
+    var app = builder.Build();
+    app.Start();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+catch (Exception ex)
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    Log.Error(ex, "Application terminated unexpectedly");
+}
+finally
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    Log.CloseAndFlush();
 }
